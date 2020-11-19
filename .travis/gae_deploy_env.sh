166f4eb18e8b530e53a256f0001e59f258611cdf
Timon Borter <timon.borter@gmx.ch>
2020-11-19 22:42:49 +0100

    #76 move project authentication into deploy_env script.

--- .travis/gae_deploy_env.sh
+++ .travis/gae_deploy_env.sh
@@ -5,4 +5,5 @@
 # Deploy event-planner go Google App Engine. Environment based (`canary` or `release`).
 # Usage: `./gae_deploy_env.sh <environment> <project-name> <docker-tag>`
 
+gcloud config set project $2
 gcloud app deploy app-$1.yml --version=$3 --image-url=gcr.io/$2/event-planner:$3
