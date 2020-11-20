a96e72c3b6cb22c128c2004e69a02d12e55408b2
Timon Borter <timon.borter@gmx.ch>
2020-11-20 08:06:40 +0100

    #76  use quiet deployment. one cannot approve in ci/cd.

--- .travis/gae_deploy_env.sh
+++ .travis/gae_deploy_env.sh
@@ -5,5 +5,4 @@
 # Deploy event-planner go Google App Engine. Environment based (`canary` or `release`).
 # Usage: `./gae_deploy_env.sh <environment> <project-name> <docker-tag>`
 
-gcloud config set project $2
-gcloud app deploy app-$1.yml --version=$3 --image-url=gcr.io/$2/event-planner:$3
+gcloud app deploy app-$1.yml --version=$3 --image-url=gcr.io/$2/event-planner:$3 --quiet
