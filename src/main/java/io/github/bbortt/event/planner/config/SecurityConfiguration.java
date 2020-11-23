6533f706909ceb77fb2948444296422201eabaaa
Timon Borter <timon.borter@gmx.ch>
2020-11-23 17:45:50 +0100

    reenable info endpoint. frontend must query active profiles.

--- src/main/java/io/github/bbortt/event/planner/config/SecurityConfiguration.java
+++ src/main/java/io/github/bbortt/event/planner/config/SecurityConfiguration.java
@@ -87,6 +87,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
+            .antMatchers("/management/info").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .and()
             .httpBasic()
