da5c0c066452bd2f1051f2d78178445c05e2e8bb
Timon Borter <timon.borter@gmx.ch>
2020-12-03 14:18:26 +0100

    #95 fix user resource urls.

--- src/main/webapp/app/core/user/user.service.ts
+++ src/main/webapp/app/core/user/user.service.ts
@@ -34,10 +34,10 @@ export class UserService {
   }
 
   authorities(): Observable<string[]> {
-    return this.http.get<string[]>(`${this.resourceUrl}/users/authorities`);
+    return this.http.get<string[]>(`${this.resourceUrl}/authorities`);
   }
 
   findByEmailOrLogin(emailOrLogin: string): Observable<User[]> {
-    return this.http.post<User[]>(`${this.resourceUrl}/users/findByEmailOrLogin`, { emailOrLogin });
+    return this.http.post<User[]>(`${this.resourceUrl}/findByEmailOrLogin`, { emailOrLogin });
   }
 }
