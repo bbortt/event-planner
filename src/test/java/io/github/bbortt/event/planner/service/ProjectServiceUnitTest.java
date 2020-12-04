bebe2b0f34a977e6043722a63d00627a3d25f430
Timon Borter <timon.borter@gmx.ch>
2020-12-04 09:17:39 +0100

    #39 backend project time validation.

--- src/test/java/io/github/bbortt/event/planner/service/ProjectServiceUnitTest.java
+++ src/test/java/io/github/bbortt/event/planner/service/ProjectServiceUnitTest.java
@@ -1,5 +1,10 @@
 package io.github.bbortt.event.planner.service;
 
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
@@ -8,6 +13,9 @@ import io.github.bbortt.event.planner.repository.ProjectRepository;
 import io.github.bbortt.event.planner.repository.RoleRepository;
 import io.github.bbortt.event.planner.repository.UserRepository;
 import io.github.bbortt.event.planner.security.AuthoritiesConstants;
+import io.github.bbortt.event.planner.service.dto.CreateProjectDTO;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import org.junit.Before;
 import org.junit.Rule;
@@ -22,9 +30,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.TestSecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
 import org.springframework.test.context.TestExecutionListeners;
+import org.zalando.problem.Status;
+import org.zalando.problem.violations.ConstraintViolationProblem;
 
 @TestExecutionListeners({ WithSecurityContextTestExecutionListener.class })
 public class ProjectServiceUnitTest {
+
     static final String MOCK_USER_LOGIN = "mock-user-login";
 
     @Rule
@@ -86,5 +97,30 @@ public class ProjectServiceUnitTest {
         fixture.findMineOrAll(Boolean.TRUE, pageable);
 
         verify(projectRepositoryMock).findAll(pageable);
+    }
+
+    @Test
+    public void createThrowsErrorOnInvalidStartEndTimeCombination() {
+        ZonedDateTime startTime = ZonedDateTime.now();
+        ZonedDateTime endTime = startTime.minus(1, ChronoUnit.MINUTES);
+
+        CreateProjectDTO createProjectDTO = new CreateProjectDTO();
+        createProjectDTO.setStartTime(startTime);
+        createProjectDTO.setEndTime(endTime);
+
+        expectedException.expect(ConstraintViolationProblem.class);
+        expectedException.expect(hasProperty("status", is(equalTo(Status.BAD_REQUEST))));
+        expectedException.expect(
+            hasProperty(
+                "violations",
+                contains(
+                    allOf(
+                        hasProperty("field", is(equalTo("endTime"))),
+                        hasProperty("message", is(equalTo("endTime may not occur before startTime!")))
+                    )
+                )
+            )
+        );
+        fixture.create(createProjectDTO);
     }
 }
