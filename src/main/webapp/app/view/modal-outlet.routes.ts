4e8113a863deb3c9343b91bf89ab18d5459baf23
Sven De Gasparo <sven.degasparo@students.bfh.ch>
2020-12-11 11:27:00 +0100

    Prepare modal for section

--- src/main/webapp/app/view/modal-outlet.routes.ts
+++ src/main/webapp/app/view/modal-outlet.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { ProjectResolve } from 'app/entities/project/project.resolve';
 import { ResponsibilityResolve } from 'app/entities/responsibility/responsibility.resolve';
 import { LocationResolve } from 'app/entities/location/location.resolve';
+import { SectionResolve } from 'app/entities/section/section.resolve';
 
 import { CreateProjectModalComponent } from 'app/view/create-project/create-project-modal.component';
 import { ProjectResponsibilityModalComponent } from 'app/view/project/admin/responsibilities/project-responsibility-modal.component';
 import { ProjectUserModalComponent } from 'app/view/project/admin/users/project-user-modal.component';
-
 import { ProjectLocationModalComponent } from 'app/view/project/admin/locations/project-location-modal.component';
+import { ProjectSectionModalComponent } from 'app/view/project/admin/locations/sections/project-section-modal.component';
 
 export const MODAL_OUTLET_ROUTES: Routes = [
   {
@@ -54,6 +55,25 @@ export const MODAL_OUTLET_ROUTES: Routes = [
     resolve: {
       project: ProjectResolve,
       location: LocationResolve,
+    },
+    outlet: 'modal',
+  },
+  {
+    path: 'projects/:projectId/locations/:locationId/sections/new',
+    component: ProjectSectionModalComponent,
+    resolve: {
+      project: ProjectResolve,
+      location: LocationResolve,
+    },
+    outlet: 'modal',
+  },
+  {
+    path: 'projects/:projectId/locations/:locationId/sections/:sectionId/edit',
+    component: ProjectSectionModalComponent,
+    resolve: {
+      project: ProjectResolve,
+      location: LocationResolve,
+      section: SectionResolve,
     },
     outlet: 'modal',
   },
---
+++ src/main/webapp/app/view/project/admin/locations/sections/project-section-modal.component.ts
@@ -0,0 +1,39 @@
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { JhiEventManager } from 'ng-jhipster';
+import { ProjectSectionUpdateComponent } from 'app/view/project/admin/locations/sections/project-section-update.component';
+
+@Component({
+  template: '',
+})
+export class ProjectSectionModalComponent implements AfterViewInit, OnDestroy {
+  private modalRef?: NgbModalRef;
+
+  constructor(private modalService: NgbModal, private eventManager: JhiEventManager, private activatedRoute: ActivatedRoute) {}
+
+  ngAfterViewInit(): void {
+    this.activatedRoute.data.subscribe(({ project, location, section }) => {
+      this.modalRef = this.modalService.open(ProjectSectionModalComponent, {
+        beforeDismiss(): boolean {
+          window.history.back();
+          return true;
+        },
+      });
+      (this.modalRef.componentInstance as ProjectSectionUpdateComponent).updateForm(
+        project,
+        location ? location : {},
+        section ? section : {}
+      );
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.modalRef) {
+      this.modalRef.close();
+    }
+
+    this.eventManager.broadcast('sectionListModification');
+  }
+}
