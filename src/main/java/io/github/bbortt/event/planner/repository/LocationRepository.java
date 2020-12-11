abbfc34cd0894876893ccc8666f2c50810ba6daa
Timon Borter <timon.borter@gmx.ch>
2020-12-11 08:58:38 +0100

    attempt to solve n+1 query problem when paginating locations per project.

--- src/main/java/io/github/bbortt/event/planner/repository/LocationRepository.java
+++ src/main/java/io/github/bbortt/event/planner/repository/LocationRepository.java
@@ -1,11 +1,8 @@
 package io.github.bbortt.event.planner.repository;
 
 import io.github.bbortt.event.planner.domain.Location;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
-    @EntityGraph(attributePaths = { "sections" })
-    @Fetch(FetchMode.JOIN)
     Page<Location> findAllByProjectId(Long projectId, Pageable pageable);
 }
--- src/main/java/io/github/bbortt/event/planner/service/LocationService.java
+++ src/main/java/io/github/bbortt/event/planner/service/LocationService.java
@@ -2,6 +2,7 @@ package io.github.bbortt.event.planner.service;
 
 import io.github.bbortt.event.planner.domain.Location;
 import io.github.bbortt.event.planner.repository.LocationRepository;
+import io.github.bbortt.event.planner.repository.SectionRepository;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,9 +21,11 @@ public class LocationService {
     private final Logger log = LoggerFactory.getLogger(LocationService.class);
 
     private final LocationRepository locationRepository;
+    private final SectionRepository sectionRepository;
 
-    public LocationService(LocationRepository locationRepository) {
+    public LocationService(LocationRepository locationRepository, SectionRepository sectionRepository) {
         this.locationRepository = locationRepository;
+        this.sectionRepository = sectionRepository;
     }
 
     /**
@@ -70,7 +73,27 @@ public class LocationService {
         locationRepository.deleteById(id);
     }
 
+    /**
+     * Find all Locations for the given project.
+     *
+     * @param projectId the project to retrieve locations for.
+     * @param pageable  the pagination information.
+     * @return the list of entities.
+     */
     public Page<Location> findAllByProjectId(Long projectId, Pageable pageable) {
-        return locationRepository.findAllByProjectId(projectId, pageable);
+        log.debug("Request to get all Locations for Project {}", projectId);
+        return locationRepository.findAllByProjectId(projectId, pageable).map(this::fetchSections);
+    }
+
+    /**
+     * Resolve section entity graph based on given location.
+     *
+     * @param location the location.
+     * @return the enriched location.
+     */
+    private Location fetchSections(Location location) {
+        log.debug("Enrich location {} with sections", location.getId());
+        location.sections(sectionRepository.findAllByLocationId(location.getId()));
+        return location;
     }
 }
