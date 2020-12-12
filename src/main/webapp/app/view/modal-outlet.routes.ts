547f584024c87971471611d3e9ee31a2119b3f9c
Sven De Gasparo <sven.degasparo@students.bfh.ch>
2020-12-12 10:47:44 +0100

    Restrict access to the modal

--- src/main/webapp/app/view/modal-outlet.routes.ts
+++ src/main/webapp/app/view/modal-outlet.routes.ts
@@ -8,6 +8,8 @@ import { ProjectAdminUpdateModalComponent } from 'app/view/project/admin/project
 import { ProjectResponsibilityModalComponent } from 'app/view/project/admin/responsibilities/project-responsibility-modal.component';
 import { ProjectUserModalComponent } from 'app/view/project/admin/users/project-user-modal.component';
 import { ProjectLocationModalComponent } from 'app/view/project/admin/locations/project-location-modal.component';
+import { ADMIN, SECRETARY } from 'app/shared/constants/role.constants';
+import { UserRouteRoleAccessService } from 'app/core/auth/user-route-role-access-service';
 
 export const MODAL_OUTLET_ROUTES: Routes = [
   {
@@ -18,6 +20,10 @@ export const MODAL_OUTLET_ROUTES: Routes = [
   {
     path: 'projects/:projectId/edit',
     component: ProjectAdminUpdateModalComponent,
+    data: {
+      roles: [ADMIN.name, SECRETARY.name],
+    },
+    canActivate: [UserRouteRoleAccessService],
     resolve: {
       project: ProjectResolve,
     },
@@ -26,6 +32,10 @@ export const MODAL_OUTLET_ROUTES: Routes = [
   {
     path: 'projects/:projectId/responsibilities/new',
     component: ProjectResponsibilityModalComponent,
+    data: {
+      roles: [ADMIN.name, SECRETARY.name],
+    },
+    canActivate: [UserRouteRoleAccessService],
     resolve: {
       project: ProjectResolve,
     },
@@ -34,6 +44,10 @@ export const MODAL_OUTLET_ROUTES: Routes = [
   {
     path: 'projects/:projectId/responsibilities/:responsibilityId/edit',
     component: ProjectResponsibilityModalComponent,
+    data: {
+      roles: [ADMIN.name, SECRETARY.name],
+    },
+    canActivate: [UserRouteRoleAccessService],
     resolve: {
       project: ProjectResolve,
       responsibility: ResponsibilityResolve,
@@ -43,6 +57,10 @@ export const MODAL_OUTLET_ROUTES: Routes = [
   {
     path: 'projects/:projectId/users/invite',
     component: ProjectUserModalComponent,
+    data: {
+      roles: [ADMIN.name, SECRETARY.name],
+    },
+    canActivate: [UserRouteRoleAccessService],
     resolve: {
       project: ProjectResolve,
     },
@@ -51,6 +69,10 @@ export const MODAL_OUTLET_ROUTES: Routes = [
   {
     path: 'projects/:projectId/locations/new',
     component: ProjectLocationModalComponent,
+    data: {
+      roles: [ADMIN.name, SECRETARY.name],
+    },
+    canActivate: [UserRouteRoleAccessService],
     resolve: {
       project: ProjectResolve,
     },
@@ -59,6 +81,10 @@ export const MODAL_OUTLET_ROUTES: Routes = [
   {
     path: 'projects/:projectId/locations/:locationId/edit',
     component: ProjectLocationModalComponent,
+    data: {
+      roles: [ADMIN.name, SECRETARY.name],
+    },
+    canActivate: [UserRouteRoleAccessService],
     resolve: {
       project: ProjectResolve,
       location: LocationResolve,
