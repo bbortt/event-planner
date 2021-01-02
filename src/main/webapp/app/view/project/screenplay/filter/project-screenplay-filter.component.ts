ebc6fe1fa186ed74bea6ca77dbd53be81468da5f
Timon Borter <timon.borter@gmx.ch>
2021-01-02 11:34:07 +0100

    #12 expand/collapse all button in screenplay nav.

--- src/main/webapp/app/view/project/screenplay/filter/project-screenplay-filter.component.ts
+++ src/main/webapp/app/view/project/screenplay/filter/project-screenplay-filter.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-project-screenplay-filter',
   templateUrl: './project-screenplay-filter.component.html',
   styleUrls: ['project-screenplay-filter.component.scss'],
 })
-export class ProjectScreenplayFilterComponent {}
+export class ProjectScreenplayFilterComponent implements OnInit, OnDestroy {
+  @Input()
+  allExpanded?: EventEmitter<boolean>;
+
+  @Output()
+  onExpandAll = new EventEmitter<boolean>();
+
+  chevronDown = faChevronDown;
+  chevronUp = faChevronUp;
+
+  locationsExpanded = false;
+
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.allExpanded!.pipe(takeUntil(this.destroy$)).subscribe((allExpanded: boolean) => (this.locationsExpanded = allExpanded));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  expandLocations(): void {
+    this.locationsExpanded = !this.locationsExpanded;
+    this.onExpandAll.emit(this.locationsExpanded);
+  }
+}
--- src/main/webapp/app/view/project/screenplay/project-screenplay.component.ts
+++ src/main/webapp/app/view/project/screenplay/project-screenplay.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { HttpResponse } from '@angular/common/http';
+
+import { Subject } from 'rxjs';
 
 import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 
@@ -12,6 +14,7 @@ import { LocationService } from 'app/entities/location/location.service';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 import { ADMIN, SECRETARY } from 'app/shared/constants/role.constants';
+import { takeUntil } from 'rxjs/operators';
 
 const ROUTE_PARAM_NAME = 'activeLocations';
 
@@ -20,7 +23,7 @@ const ROUTE_PARAM_NAME = 'activeLocations';
   templateUrl: './project-screenplay.component.html',
   styleUrls: ['project-screenplay.component.scss'],
 })
-export class ProjectScreenplayComponent implements OnInit {
+export class ProjectScreenplayComponent implements OnInit, OnDestroy {
   faCog = faCog;
 
   adminRoles = [ADMIN.name, SECRETARY.name];
@@ -29,19 +32,23 @@ export class ProjectScreenplayComponent implements OnInit {
   locations?: Location[];
 
   activeLocations: string[] = [];
+  allExpanded = new EventEmitter<boolean>();
+
+  private destroy$ = new Subject();
 
   constructor(private locationService: LocationService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ project }) => {
+    this.activatedRoute.data.pipe(takeUntil(this.destroy$)).subscribe(({ project }) => {
       this.project = project;
 
-      this.locationService
-        .findAllByProject(this.project!, { sort: ['name,asc'] })
-        .subscribe((response: HttpResponse<Location[]>) => (this.locations = response.body || []));
+      this.locationService.findAllByProject(this.project!, { sort: ['name,asc'] }).subscribe((response: HttpResponse<Location[]>) => {
+        this.locations = response.body || [];
+        this.afterActiveLocationsChange();
+      });
     });
 
-    this.activatedRoute.queryParams.subscribe((params: Params) => {
+    this.activatedRoute.queryParams.pipe(takeUntil(this.destroy$)).subscribe((params: Params) => {
       let activeIds = params[ROUTE_PARAM_NAME];
       if (activeIds) {
         activeIds = JSON.parse(activeIds);
@@ -49,13 +56,19 @@ export class ProjectScreenplayComponent implements OnInit {
           activeIds = [activeIds];
         }
         this.activeLocations = activeIds;
+        this.afterActiveLocationsChange();
       }
     });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   reset(): void {
     this.activeLocations = [];
-    this.pushParams();
+    this.afterActiveLocationsChange();
   }
 
   onPanelChange(event: NgbPanelChangeEvent): void {
@@ -67,14 +80,32 @@ export class ProjectScreenplayComponent implements OnInit {
       this.activeLocations.splice(this.activeLocations.indexOf(panelId), 1);
     }
 
-    this.pushParams();
+    this.afterActiveLocationsChange();
   }
 
   isActiveId(id: string): boolean {
     return this.activeLocations.includes(id);
   }
 
-  private pushParams(): void {
+  onExpandAll(expanded: boolean): void {
+    if (expanded) {
+      this.activeLocations = this.locations!.map((location: Location) => `location-${location.id!}`);
+    } else {
+      this.activeLocations = [];
+    }
+
+    this.afterActiveLocationsChange();
+  }
+
+  private afterActiveLocationsChange(): void {
+    // Control screenplay nav buttons
+    if (this.activeLocations.length === 0) {
+      this.allExpanded.emit(false);
+    } else if (this.activeLocations.length === this.locations?.length) {
+      this.allExpanded.emit(true);
+    }
+
+    // Update route
     this.router.navigate(['.'], {
       relativeTo: this.activatedRoute,
       queryParams: { [ROUTE_PARAM_NAME]: JSON.stringify(this.activeLocations) },
