550dfad6264954f46d388010299ff61756f579e5
Timon Borter <timon.borter@gmx.ch>
2021-01-12 16:46:35 +0100

    #88 responsibility resource - revert invalid formatting changes.

--- src/main/java/io/github/bbortt/event/planner/web/rest/ResponsibilityResource.java
+++ src/main/java/io/github/bbortt/event/planner/web/rest/ResponsibilityResource.java
@@ -57,9 +57,7 @@ public class ResponsibilityResource {
      * {@code POST  /responsibilities} : Create a new responsibility.
      *
      * @param responsibility the responsibility to create.
-     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new
-     * responsibility, or with status {@code 400 (Bad Request)} if the responsibility has already an
-     * ID.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new responsibility, or with status {@code 400 (Bad Request)} if the responsibility has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
     @PostMapping("/responsibilities")
@@ -87,10 +85,7 @@ public class ResponsibilityResource {
      * {@code PUT  /responsibilities} : Updates an existing responsibility.
      *
      * @param responsibility the responsibility to update.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated
-     * responsibility, or with status {@code 400 (Bad Request)} if the responsibility is not valid,
-     * or with status {@code 500 (Internal Server Error)} if the responsibility couldn't be
-     * updated.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated responsibility, or with status {@code 400 (Bad Request)} if the responsibility is not valid, or with status {@code 500 (Internal Server Error)} if the responsibility couldn't be updated.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
     @PutMapping("/responsibilities")
@@ -118,8 +113,7 @@ public class ResponsibilityResource {
      * {@code GET  /responsibilities} : get all the responsibilities.
      *
      * @param pageable the pagination information.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of
-     * responsibilities in body.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of responsibilities in body.
      */
     @GetMapping("/responsibilities")
     @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.ADMIN + "\")")
@@ -134,8 +128,7 @@ public class ResponsibilityResource {
      * {@code GET  /responsibilities/:id} : get the "id" responsibility.
      *
      * @param id the id of the responsibility to retrieve.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the
-     * responsibility, or with status {@code 404 (Not Found)}.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the responsibility, or with status {@code 404 (Not Found)}.
      */
     @GetMapping("/responsibilities/{id}")
     @PreAuthorize(
@@ -162,11 +155,10 @@ public class ResponsibilityResource {
     }
 
     /**
-     * {@code POST /responsibilities/project/:projectId/name-exists} : Whether the given name is
-     * still unique in this Project.
+     * {@code POST /responsibilities/project/:projectId/name-exists} : Whether the given name is still unique in this Project.
      *
      * @param projectId the Project identifier.
-     * @param name      the value to check.
+     * @param name the value to check.
      * @return true if the value exists.
      */
     @PostMapping("/responsibilities/project/{projectId}/name-exists")
