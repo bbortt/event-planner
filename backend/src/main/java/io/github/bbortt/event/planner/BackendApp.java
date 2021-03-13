873a32bb5feb57a2dad3ac270775048a3516a470
Timon Borter <timon.borter@gmx.ch>
2021-03-13 01:12:55 +0100

    ready to copy sources?

--- backend/src/main/java/io/github/bbortt/event/planner/BackendApp.java
+++ backend/src/main/java/io/github/bbortt/event/planner/BackendApp.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 
@@ -21,7 +20,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 @SpringBootApplication
-@EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
+@EnableConfigurationProperties({ApplicationProperties.class})
 public class BackendApp {
 
     private static final Logger log = LoggerFactory.getLogger(BackendApp.class);
