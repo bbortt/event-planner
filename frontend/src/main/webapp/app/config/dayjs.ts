757d82ee7c655bf24bb2fdd3adc767643d519f7d
Timon Borter <timon.borter@gmx.ch>
2021-07-15 20:08:07 +0200

    #231 update frontend dependencies (angular v12). tests not working: jasmine not found?

--- frontend/src/main/webapp/app/config/dayjs.ts
+++ frontend/src/main/webapp/app/config/dayjs.ts
@@ -3,11 +3,9 @@ import * as customParseFormat from 'dayjs/plugin/customParseFormat';
 import * as duration from 'dayjs/plugin/duration';
 import * as relativeTime from 'dayjs/plugin/relativeTime';
 
-// jhipster-needle-i18n-language-dayjs-imports - JHipster will import languages from dayjs here
 import 'dayjs/locale/de';
 import 'dayjs/locale/en';
 
-// DAYJS CONFIGURATION
 dayjs.extend(customParseFormat);
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
