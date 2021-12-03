4d0b558c8bae7b5ed5a65303c63d29d8941444d6
Timon Borter <timon.borter@gmx.ch>
2021-12-03 23:15:44 +0100

    fix(wiremock): wiremock evaluates templates
    with the spring customization, wiremock finally evaluates teamplate
    variables again. re-enable integration tests in build pipeline.

--- src/test/java/io/github/bbortt/event/planner/config/WireMockCustomizerConfig.java
+++ src/test/java/io/github/bbortt/event/planner/config/WireMockCustomizerConfig.java
@@ -1,14 +1,15 @@
 package io.github.bbortt.event.planner.config;
 
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import org.springframework.cloud.contract.wiremock.WireMockConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class WireMockTemplateTransformerConfig {
+public class WireMockCustomizerConfig {
 
   @Bean
-  public ResponseTemplateTransformer responseTemplateTransformer() {
-    return new ResponseTemplateTransformer(true);
+  WireMockConfigurationCustomizer optionsCustomizer() {
+    return options -> options.extensions(new ResponseTemplateTransformer(true));
   }
 }
