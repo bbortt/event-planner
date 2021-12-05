7ebb6fb8ca4a0d023afd34dc442f9570994855bd
Timon Borter <timon.borter@gmx.ch>
2021-12-05 15:56:40 +0100

    chore: elk stack deployment on kubernetes

---
+++ deployment/postgresql/pool/init/enable-uuid-extension.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# This scripts executes init.sql which makes sure the uuid-ossp extension is enabled.
+
+PGPASSWORD=$PGPOOL_POSTGRES_PASSWORD psql \
+  -U $PGPOOL_POSTGRES_USERNAME \
+  -d event_planner \
+  -h pg-cluster.${K8S_NAMESPACE}.svc.cluster.local \
+  -a \
+  -f "/docker-entrypoint-initdb.d/init.sql"
