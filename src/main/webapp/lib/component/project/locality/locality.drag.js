df434f618240a77ccb0ccc572d2bda998cf03e5f
Timon Borter <timon.borter@gmx.ch>
2022-02-08 22:33:52 +0100

    chore: root locality drag 'n drop

--- src/main/webapp/lib/component/project/locality/locality.drag.js
+++ src/main/webapp/lib/component/project/locality/locality.drag.js
@@ -1,28 +1,55 @@
 // @flow
 import * as React from 'react';
 
-import { useDrag } from 'react-dnd';
+import { useDrag, useDrop } from 'react-dnd';
 
 import styles from './locality.drag.module.scss';
 
+export const LOCALITY_DROP_TYPE = 'locality-drop';
+
 export type localityDragPropTypes = {
   locality: Locality,
+  onClick: (locality: Locality) => void,
 };
 
-export const LocalityDrag = ({ locality }: localityDragPropTypes): React.Element<'div'> => {
+export const LocalityDrag = ({ locality, onClick }: localityDragPropTypes): React.Element<'div'> => {
   const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
-    // "type" is required. It is used by the "accept" specification of drop targets.
-    type: 'BOX',
-    // The collect function utilizes a "monitor" instance (see the Overview for what this is)
-    // to pull important pieces of state from the DnD system.
+    type: LOCALITY_DROP_TYPE,
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
+    item: { ...locality },
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult();
+      if (item && dropResult && item.id !== dropResult.id) {
+        console.log(`You dropped ${item} into ${dropResult}!`);
+      }
+    },
+  }));
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: LOCALITY_DROP_TYPE,
+    collect: monitor => ({
+      canDrop: monitor.canDrop(),
+      isOver: monitor.isOver(),
+    }),
+    drop: () => locality,
   }));
 
   return (
-    <div ref={dragPreview} style={{ opacity: isDragging ? 0.5 : 1 }} className={`locality-drag ${styles.localityDrag}`}>
-      <div role="Handle" ref={drag} />
+    <div
+      className={`locality-drag hoverable ${styles.localityDrag} ${!isDragging && canDrop ? styles.localityDragTarget : ''} ${
+        !isDragging && isOver ? styles.localityDragActive : ''
+      }`}
+      onClick={() => onClick(locality)}
+      ref={drop}
+      role={'Dustbin'}
+    >
+      <div ref={dragPreview} style={{ opacity: isDragging ? 0.7 : 1 }}>
+        <div className="grid-x" ref={drag} role="Handle">
+          <div className="cell small-6">Lokalität "{locality.name}"</div>
+          <div className="cell small-6 text-right">&#187;</div>
+        </div>
+      </div>
     </div>
   );
 };
