5637025409c0b2005a37125ee5c844ba9c550c4e
Timon Borter <timon.borter@gmx.ch>
2022-02-23 21:32:27 +0100

    ci: include jest reports in sonar

--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,18 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig = {
+  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/main/webapp/*.js'],
+  coverageDirectory: '<rootDir>/build/test-results/',
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
+  reporters: ['default', ['jest-junit', { outputDirectory: '<rootDir>/build/test-results/', outputName: 'TESTS-results-jest.xml' }]],
   setupFilesAfterEnv: ['<rootDir>/src/test/js/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/.owl/', '<rootDir>/node_modules/', '<rootDir>/src/test/e2e/'],
+  testResultsProcessor: 'jest-sonar-reporter',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { ...babelConfig }],
   },
