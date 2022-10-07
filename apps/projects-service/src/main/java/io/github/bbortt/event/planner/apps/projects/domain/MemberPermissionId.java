3a6b64d62efb05e4faada03fe7f213d688e516fa
Timon Borter <timon.borter@gmx.ch>
2022-10-07 09:37:16 +0200

    fix: sonar code smells

--- apps/projects-service/src/main/java/io/github/bbortt/event/planner/apps/projects/domain/MemberPermissionId.java
+++ apps/projects-service/src/main/java/io/github/bbortt/event/planner/apps/projects/domain/MemberPermissionId.java
@@ -1,10 +1,19 @@
 package io.github.bbortt.event.planner.apps.projects.domain;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Serial;
 import java.io.Serializable;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class MemberPermissionId implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  private final transient ObjectMapper objectMapper = new ObjectMapper();
 
   private Member member;
   private Permission permission;
@@ -43,5 +52,17 @@ public class MemberPermissionId implements Serializable {
   @Override
   public int hashCode() {
     return new HashCodeBuilder(17, 37).append(getMember()).append(getPermission()).toHashCode();
+  }
+
+  @Serial
+  private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+    objectMapper.writeValue((OutputStream) out, this);
+  }
+
+  @Serial
+  private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+    MemberPermissionId memberPermissionId = objectMapper.readValue(in.readAllBytes(), MemberPermissionId.class);
+    setMember(memberPermissionId.getMember());
+    setPermission(memberPermissionId.getPermission());
   }
 }
