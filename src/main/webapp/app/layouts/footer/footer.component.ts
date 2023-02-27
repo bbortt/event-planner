3f804d682f440c8df4b270e5810aceb7ecaa542d
Timon Borter <timon.borter@gmx.ch>
2023-02-27 20:29:10 +0100

    chore: update logo and imaging of application

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
+import { IconLookup } from '@fortawesome/fontawesome-common-types';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public faGithub = faGithub as IconLookup;
+}
