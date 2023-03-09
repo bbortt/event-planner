435620cc51d2adda7fc5b17156cb7df69d1f7a37
Timon Borter <timon.borter@gmx.ch>
2023-03-09 20:02:56 +0100

    fix(frontend): project service tests

--- src/main/webapp/app/entities/project/project.test-samples.ts
+++ src/main/webapp/app/entities/project/project.test-samples.ts
@@ -6,8 +6,8 @@ export const sampleWithRequiredData: IProject = {
   id: 55962,
   token: 'fd8b446f-e9a1-48c6-88cf-9e36358a8070',
   name: 'Response Investor Loan',
-  startDate: dayjs('2023-02-23T05:18'),
-  endDate: dayjs('2023-02-23T15:06'),
+  startDate: dayjs('2023-02-23'),
+  endDate: dayjs('2023-02-23'),
   archived: true,
 };
 
@@ -27,8 +27,8 @@ export const sampleWithFullData: IProject = {
   token: 'af6c4d9c-362d-4048-a7e0-9f9203bc1ed8',
   name: 'Wooden SCSI',
   description: 'Cheese',
-  startDate: dayjs('2023-02-23T12:33'),
-  endDate: dayjs('2023-02-22T19:13'),
+  startDate: dayjs('2023-02-23'),
+  endDate: dayjs('2023-02-22'),
   archived: false,
   createdBy: 'Cotton Specialist',
   createdDate: dayjs('2023-02-22T20:50'),
@@ -39,8 +39,8 @@ export const sampleWithFullData: IProject = {
 export const sampleWithNewData: NewProject = {
   token: '0eb7769e-28e0-4872-aafd-09fb60b702b8',
   name: 'Officer',
-  startDate: dayjs('2023-02-23T15:50'),
-  endDate: dayjs('2023-02-23T05:13'),
+  startDate: dayjs('2023-02-23'),
+  endDate: dayjs('2023-02-23'),
   archived: true,
   id: null,
 };
