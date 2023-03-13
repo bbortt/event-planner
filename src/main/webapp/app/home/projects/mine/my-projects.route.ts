a0c74bde433c3b6520155a4a80ef98482e74f30d
Timon Borter <timon.borter@gmx.ch>
2023-03-13 15:25:44 +0100

    fix: project modal

--- src/main/webapp/app/home/projects/mine/my-projects.route.ts
+++ src/main/webapp/app/home/projects/mine/my-projects.route.ts
@@ -1,16 +1,22 @@
 import { Route } from '@angular/router';
 
-import { ProjectCreateModalComponent } from './project-create-modal.component';
 import { MyProjectsListComponent } from './my-projects-list.component';
+import { ProjectCreateModalComponent } from './project-create-modal.component';
+import { DEFAULT_SORT_DATA } from '../../../config/navigation.constants';
 
 export const MY_PROJECTS_ROUTES: Route[] = [
   {
     path: '',
     component: MyProjectsListComponent,
+    data: {
+      pageTitle: 'home.title',
+      [DEFAULT_SORT_DATA]: 'startDate,asc',
+    },
   },
   {
-    path: 'projects/new',
+    path: 'project/new',
     component: ProjectCreateModalComponent,
     outlet: 'modal',
+    pathMatch: 'full',
   },
 ];
--- src/main/webapp/app/home/projects/projects.route.ts
+++ src/main/webapp/app/home/projects/projects.route.ts
@@ -6,9 +6,5 @@ export const MY_PROJECTS_ROUTES: Route[] = [
   {
     path: '',
     loadChildren: () => import('./mine/my-projects.module').then(m => m.MyProjectsModule),
-    data: {
-      pageTitle: 'home.title',
-      [DEFAULT_SORT_DATA]: 'startDate,asc',
-    },
   },
 ];
