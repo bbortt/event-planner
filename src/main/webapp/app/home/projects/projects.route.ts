c0fd670e0777b527a179bc1dc68c4bce014f0135
Timon Borter <timon.borter@gmx.ch>
2023-03-15 17:51:55 +0100

    chore: fix two code smells

--- src/main/webapp/app/home/projects/projects.route.ts
+++ src/main/webapp/app/home/projects/projects.route.ts
@@ -1,7 +1,5 @@
 import { Route } from '@angular/router';
 
-import { DEFAULT_SORT_DATA } from '../../config/navigation.constants';
-
 export const MY_PROJECTS_ROUTES: Route[] = [
   {
     path: '',
