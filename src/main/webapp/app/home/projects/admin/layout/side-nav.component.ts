050880b9b11cb3cf7d6ac047a10f1e19560d1fe4
Timon Borter <timon.borter@gmx.ch>
2023-03-23 18:52:41 +0100

    chore: sexy page title on navigation

--- src/main/webapp/app/home/projects/admin/layout/side-nav.component.ts
+++ src/main/webapp/app/home/projects/admin/layout/side-nav.component.ts
@@ -1,19 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 import { IProject } from '../../../../entities/project/project.model';
+import { TranslateService } from '@ngx-translate/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-project-admin-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnDestroy, OnInit {
   @Input() project: IProject | null = null;
 
   protected faLocationDot = faLocationDot;
   protected isNavbarCollapsed = true;
+
+  private routerSubscription: Subscription | null = null;
+
+  constructor(private router: Router, private titleService: Title, private translateService: TranslateService) {}
+
+  ngOnInit(): void {
+    this.routerSubscription = this.router.events
+      .pipe(filter((event: Event) => event instanceof NavigationEnd))
+      .subscribe(navigationEnd => this.updateTitleBasedOnUrl((navigationEnd as NavigationEnd).url));
+
+    this.updateTitleBasedOnUrl(this.router.url);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
 
   collapseNavbar(): void {
     this.isNavbarCollapsed = true;
@@ -21,5 +44,17 @@ export class SideNavComponent {
 
   toggleNavbar(): void {
     this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  private updateTitleBasedOnUrl(url: string): void {
+    if (url.endsWith('/admin/settings')) {
+      this.updateTitleWithTranslation('global.menu.account.settings');
+    } else if (url.endsWith('/admin/members')) {
+      this.updateTitleWithTranslation('app.member.home.title');
+    }
+  }
+
+  private updateTitleWithTranslation(key: string): void {
+    this.translateService.get(key).subscribe(translation => this.titleService.setTitle(`${translation}: ${this.project?.name}`));
   }
 }
--- src/main/webapp/app/home/projects/admin/project-admin.route.ts
+++ src/main/webapp/app/home/projects/admin/project-admin.route.ts
@@ -2,9 +2,11 @@ import { Route } from '@angular/router';
 
 import { ProjectFromParentResolveService } from './route/project-from-parent-resolve.service';
 
+import { DEFAULT_SORT_DATA } from '../../../config/navigation.constants';
+
 import { AdminLayoutComponent } from './layout/admin-layout.component';
-import { ProjectSettingsComponent } from './settings/project-settings.component';
 import { ProjectMemberListComponent } from './member/project-member-list.component';
+import { ProjectSettingsComponent } from './settings/project-settings.component';
 
 export const PROJECT_ADMIN_ROUTES: Route[] = [
   {
@@ -24,8 +26,12 @@ export const PROJECT_ADMIN_ROUTES: Route[] = [
       {
         path: 'members',
         component: ProjectMemberListComponent,
+        data: {
+          [DEFAULT_SORT_DATA]: 'acceptedDate,desc',
+        },
         resolve: { project: ProjectFromParentResolveService },
       },
     ],
+    resolve: { project: ProjectFromParentResolveService },
   },
 ];
