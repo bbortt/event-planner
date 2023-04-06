fc503f3bce8055a7b6be94d9d17855c4bfb3b1da
Timon Borter <timon.borter@gmx.ch>
2023-04-06 19:10:12 +0200

    ci: advanced membership data integrity tests

---
+++ src/test/java/io/github/bbortt/event/planner/repository/MemberRepositoryIT.java
@@ -0,0 +1,49 @@
+package io.github.bbortt.event.planner.repository;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.github.bbortt.event.planner.IntegrationTest;
+import io.github.bbortt.event.planner.domain.Member;
+import io.github.bbortt.event.planner.domain.Project;
+import io.github.bbortt.event.planner.web.rest.MemberResourceIT;
+import io.github.bbortt.event.planner.web.rest.ProjectResourceIT;
+import java.util.Objects;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.transaction.annotation.Transactional;
+
+@IntegrationTest
+class MemberRepositoryIT {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void uniqueInvitationPerProject() {
+        Project project = ProjectResourceIT.createEntity(entityManager);
+        entityManager.persist(project);
+
+        memberRepository.saveAndFlush(createNewMember(project));
+
+        DataIntegrityViolationException exception = assertThrows(
+            DataIntegrityViolationException.class,
+            () -> memberRepository.saveAndFlush(createNewMember(project))
+        );
+        assertTrue(Objects.requireNonNull(exception.getMessage()).contains("ux_invitation_per_project"));
+    }
+
+    private Member createNewMember(Project project) {
+        return MemberResourceIT
+            .createEntity(entityManager)
+            .invitedEmail("victor-von-doom@localhost")
+            .accepted(Boolean.FALSE)
+            .project(project);
+    }
+}
---
+++ src/test/java/io/github/bbortt/event/planner/service/MemberServiceIT.java
@@ -0,0 +1,96 @@
+package io.github.bbortt.event.planner.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.github.bbortt.event.planner.IntegrationTest;
+import io.github.bbortt.event.planner.domain.Member;
+import io.github.bbortt.event.planner.domain.Project;
+import io.github.bbortt.event.planner.repository.MemberRepository;
+import io.github.bbortt.event.planner.service.dto.MemberDTO;
+import io.github.bbortt.event.planner.service.dto.ProjectDTO;
+import io.github.bbortt.event.planner.web.rest.MemberResourceIT;
+import io.github.bbortt.event.planner.web.rest.ProjectResourceIT;
+import java.util.UUID;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.transaction.annotation.Transactional;
+
+@IntegrationTest
+@WithMockUser("member-service-it")
+class MemberServiceIT {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MemberService memberService;
+
+    private Project project1;
+    private Project project2;
+
+    @BeforeEach
+    void beforeEachSetup() {
+        project1 = ProjectResourceIT.createEntity(entityManager).token(UUID.randomUUID());
+        entityManager.persist(project1);
+
+        project2 = ProjectResourceIT.createEntity(entityManager).token(UUID.randomUUID());
+        entityManager.persist(project2);
+    }
+
+    @Test
+    @Transactional
+    void findInProjectReturnsMembersInProject() {
+        memberRepository.save(
+            MemberResourceIT.createEntity(entityManager).invitedEmail("victor-von-doom@localhost").accepted(Boolean.FALSE).project(project1)
+        );
+        memberRepository.save(
+            MemberResourceIT.createEntity(entityManager).invitedEmail("john-walker@localhost").accepted(Boolean.TRUE).project(project1)
+        );
+        memberRepository.save(
+            MemberResourceIT.createEntity(entityManager).invitedEmail("victor-von-doom@localhost").accepted(Boolean.FALSE).project(project2)
+        );
+        memberRepository.save(
+            MemberResourceIT.createEntity(entityManager).invitedEmail("john-walker@localhost").accepted(Boolean.TRUE).project(project2)
+        );
+
+        Page<MemberDTO> members = memberService.findInProject(project1.getId(), Pageable.ofSize(3));
+
+        assertEquals(1, members.getTotalPages());
+        assertEquals(2, members.getTotalElements());
+    }
+
+    @Test
+    @Transactional
+    void inviteToProjectPersistsNewEntity() {
+        String email = "kyle-brock@localhost";
+
+        assertFalse(findByEmail(email));
+
+        ProjectDTO projectDTO = new ProjectDTO();
+        projectDTO.setId(project1.getId());
+
+        MemberDTO memberDTO = memberService.inviteToProject(email, projectDTO);
+
+        assertEquals(email, memberDTO.getInvitedEmail());
+        assertEquals(Boolean.FALSE, memberDTO.getAccepted());
+        assertEquals(projectDTO, memberDTO.getProject());
+
+        assertTrue(memberRepository.findById(memberDTO.getId()).isPresent());
+        assertTrue(findByEmail(email));
+    }
+
+    private boolean findByEmail(String email) {
+        return memberRepository.findOne(Example.of(new Member().invitedEmail(email))).isPresent();
+    }
+}
