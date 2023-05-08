79a3b731447d662f10f158eeeb87339171a4816c
Timon Borter <timon.borter@gmx.ch>
2023-05-08 21:52:38 +0200

    feat: locations drag n drop with ng2-dragula

--- src/main/webapp/app/home/projects/admin/locations/project-locations.component.ts
+++ src/main/webapp/app/home/projects/admin/locations/project-locations.component.ts
@@ -1,16 +1,25 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Project } from '../../../../api';
-import { tap } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+
+import { tap } from 'rxjs';
+
+import { GetProjectLocations200Response, Location, Project, ProjectLocationService } from 'app/api';
 
 @Component({
   selector: 'app-project-locations',
   templateUrl: './project-locations.component.html',
+  styleUrls: ['./project-locations.component.scss'],
 })
-export class ProjectLocationsComponent implements OnDestroy, OnInit {
+export class ProjectLocationsComponent implements OnInit {
   project: Project | null = null;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  locations?: Location[];
+  isLoading = false;
+
+  activeLocation: Location | null = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private projectLocationService: ProjectLocationService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data
@@ -24,7 +33,26 @@ export class ProjectLocationsComponent implements OnDestroy, OnInit {
       .subscribe(() => this.load());
   }
 
-  ngOnDestroy(): void {}
+  setActiveLocation(location: Location): void {
+    this.activeLocation = location;
+  }
 
-  private load() {}
+  private load(): void {
+    this.projectLocationService
+      .getProjectLocations(this.project!.id!, 'response')
+      .pipe(tap(() => (this.isLoading = false)))
+      .subscribe({
+        next: (res: HttpResponse<GetProjectLocations200Response>) => {
+          this.onResponseSuccess(res);
+        },
+      });
+  }
+
+  private onResponseSuccess(response: HttpResponse<GetProjectLocations200Response>): void {
+    this.locations = this.fillComponentAttributesFromResponseBody(response.body?.contents);
+  }
+
+  private fillComponentAttributesFromResponseBody(data: Array<Location> | undefined): Location[] {
+    return data ?? [];
+  }
 }
--- src/main/webapp/app/home/projects/admin/project-admin.module.ts
+++ src/main/webapp/app/home/projects/admin/project-admin.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { DragulaModule } from 'ng2-dragula';
+
 import { ProjectEntityModule } from 'app/entities/project/project-entity.module';
 
 import { SharedModule } from 'app/shared/shared.module';
@@ -9,19 +11,24 @@ import { PROJECT_ADMIN_ROUTES } from './project-admin.route';
 
 import { AdminLayoutComponent } from './layout/admin-layout.component';
 import { SideNavComponent } from './layout/side-nav.component';
+
 import { ProjectLocationsComponent } from './locations/project-locations.component';
+import { ProjectLocationDragComponent } from './locations/project-location-drag.component';
+
 import {
   ProjectMemberInviteModalComponent,
   ProjectMemberInviteModalContentComponent,
 } from './member/project-member-invite-modal.component';
 import { ProjectMemberListComponent } from './member/project-member-list.component';
+
 import { ProjectSettingsComponent } from './settings/project-settings.component';
 
 @NgModule({
-  imports: [SharedModule, ProjectEntityModule, RouterModule.forChild(PROJECT_ADMIN_ROUTES)],
+  imports: [DragulaModule.forRoot(), SharedModule, ProjectEntityModule, RouterModule.forChild(PROJECT_ADMIN_ROUTES)],
   declarations: [
     AdminLayoutComponent,
     ProjectLocationsComponent,
+    ProjectLocationDragComponent,
     ProjectMemberInviteModalComponent,
     ProjectMemberInviteModalContentComponent,
     ProjectMemberListComponent,
