6282b50313936625dcddd2107804520e037cb696
Timon Borter <timon.borter@gmx.ch>
2023-05-11 22:46:06 +0200

    chore(frontend): more drag n drop styles

--- src/main/webapp/app/home/projects/admin/locations/project-location-drag.component.ts
+++ src/main/webapp/app/home/projects/admin/locations/project-location-drag.component.ts
@@ -5,6 +5,7 @@ import { Location } from 'app/api';
 @Component({
   selector: 'app-project-location-drag',
   templateUrl: './project-location-drag.component.html',
+  styleUrls: ['./project-location-drag.component.scss'],
 })
 export class ProjectLocationDragComponent {
   @Input() location: Location | null = null;
