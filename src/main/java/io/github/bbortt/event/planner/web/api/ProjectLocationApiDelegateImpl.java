55067e56582b01a68182e14f3748631bdb4a7560
Timon Borter <timon.borter@gmx.ch>
2023-06-19 21:29:55 +0200

    chore: sonar smells cleanup

--- src/main/java/io/github/bbortt/event/planner/web/api/ProjectLocationApiDelegateImpl.java
+++ src/main/java/io/github/bbortt/event/planner/web/api/ProjectLocationApiDelegateImpl.java
@@ -9,7 +9,6 @@ import io.github.bbortt.event.planner.web.api.mapper.ApiProjectLocationMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
