8f1a09de61f18445663353f87a0a1c6cc0f7490f
Timon Borter <timon.borter@gmx.ch>
2023-09-05 19:22:56 +0200

    feat: custom month view style

--- src/main/webapp/app/home/projects/calendar/project-calendar.component.spec.ts
+++ src/main/webapp/app/home/projects/calendar/project-calendar.component.spec.ts
@@ -6,13 +6,21 @@ import { of } from 'rxjs';
 
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
+import dayjs from 'dayjs/esm';
+import utc from 'dayjs/esm/plugin/utc';
+
 import { Event, GetProjectEvents200Response, ProjectEventsService } from 'app/api';
 import { IProject } from 'app/entities/project/project.model';
 
 import { ProjectCalendarComponent } from './project-calendar.component';
 import { ProjectCalendarModule } from './project-calendar.module';
 
-const project = { id: 1234, token: 'fe156aa6-18e7-40f2-a14f-1ce93eb8d5c8' } as IProject;
+const project = {
+  id: 1234,
+  token: 'fe156aa6-18e7-40f2-a14f-1ce93eb8d5c8',
+  startDate: dayjs('2023-08-27T02:00'),
+  endDate: dayjs('2023-08-29T17:00'),
+} as IProject;
 
 describe('Project Calendar', () => {
   let projectEventsService: ProjectEventsService;
@@ -20,6 +28,11 @@ describe('Project Calendar', () => {
 
   let fixture: ComponentFixture<ProjectCalendarComponent>;
   let component: ProjectCalendarComponent;
+
+  beforeEach(() => {
+    // This would normally be done in `dayjs.ts`
+    dayjs.extend(utc);
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -70,6 +83,11 @@ describe('Project Calendar', () => {
 
       component.ngOnInit();
 
+      // @ts-ignore: force this private property value for testing.
+      expect(component.startOfProject.toString()).toEqual('Sun, 27 Aug 2023 00:00:00 GMT');
+      // @ts-ignore: force this private property value for testing.
+      expect(component.endOfProject.toString()).toEqual('Tue, 29 Aug 2023 23:59:59 GMT');
+
       expect(projectEventsService.getProjectEvents).toHaveBeenCalledWith(project.id, 0, -1, ['startDateTime,asc'], 'response');
       // @ts-ignore: force this private property value for testing.
       expect(component.events.length).toEqual(1);
@@ -79,6 +97,11 @@ describe('Project Calendar', () => {
       jest.spyOn(projectEventsService, 'getProjectEvents');
 
       component.ngOnInit();
+
+      // @ts-ignore: force this private property value for testing.
+      expect(component.startOfProject).toBeUndefined();
+      // @ts-ignore: force this private property value for testing.
+      expect(component.endOfProject).toBeUndefined();
 
       expect(projectEventsService.getProjectEvents).not.toHaveBeenCalled();
     });
