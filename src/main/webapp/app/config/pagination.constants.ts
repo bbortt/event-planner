d1fffe570547ffa3d02e5172b02a3e8963a3d376
Timon Borter <timon.borter@gmx.ch>
2023-09-10 14:49:12 +0200

    chore(frontend): project and calendar navigation

--- src/main/webapp/app/config/pagination.constants.ts
+++ src/main/webapp/app/config/pagination.constants.ts
@@ -1,4 +1,4 @@
 export const TOTAL_COUNT_RESPONSE_HEADER = 'X-Total-Count';
 export const PAGE_HEADER = 'page';
-export const ITEMS_PER_PAGE = 20;
+export const ITEMS_PER_PAGE = 10;
 export const HAS_NEXT_PAGE_HEADER = 'Has-Next-Page';
